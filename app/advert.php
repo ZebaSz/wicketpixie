2d4662115b889053140b5e0e893c562d19fad7d5
Sebastian Szperling <zebaszp@gmail.com>
2011-03-10 08:51:06 -0300

    Removed donate button, might change later

--- app/advert.php
+++ app/advert.php
@@ -1,3 +1,4 @@
+<!--
 <div id="admin-affiliates">
 	<h3>Donate to WicketPixie</h3>					
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">  
@@ -6,3 +7,4 @@
 		<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt=""> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 	</form>
 </div>
+-->
