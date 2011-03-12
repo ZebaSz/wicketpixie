7e3ea5ec875ff4578365335836acc542be815bda
Sebastian Szperling <zebaszp@gmail.com>
2011-03-12 15:29:21 -0300

    backwards compatibility with WP 2.7-2.9

--- comments.php
+++ comments.php
@@ -54,43 +54,46 @@ endif; // check for wp_list_comments ?>
 <?php else:
 if ('open' == $post->comment_status) : ?>
 	<!-- comment form -->
-	<div id="comment-form">
-		<h2 id="respond">Leave a Comment</h2>
-		<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
-		<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php the_permalink(); ?>">logged in</a> to post a comment.</p>
-		<?php else : ?>
-		<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-			<div>
-				<?php if ( $user_ID ) : ?>
-				<p class="yourname" style="width:100%;">
-					Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Logout &raquo;</a>
-				</p>
-				<?php else : ?>
-				<p class="yourname">
-					<label for="author">Your Name: <?php if ($req) echo "*"; ?></label>
-					<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="30" tabindex="1" class="inputfield" />
-				</p>
-				<p class="email">
-					<label for="email">Email Address: <?php if ($req) echo "*"; ?></label>
-					<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="30" tabindex="2" class="inputfield" />
-				</p>
-				<p class="website">
-					<label for="url">Website:</label>
-					<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="30" tabindex="3" class="inputfield" />
-				</p>
-				<?php endif; ?>
-				<p>
-					<label for="comment">Comment:</label>
-					<textarea name="comment" id="comment-message" rows="10" cols="40" class="message" tabindex="4"></textarea>
-				</p>
-				<p>
-					<input type="image" src="<?php bloginfo('template_directory'); ?>/images/button-publish.jpg" name="submit" id="comment-button" alt="Publish My Comment" />
-					<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-				</p>
-			</div>
-			<div class="clearer">&nbsp;</div>
-			<?php do_action('comment_form', $post->ID); ?>
-		</form>
+	<div id="respond">
+		<div id="comment-form">
+			<h2><?php comment_form_title(); ?></h2>
+			<p><?php cancel_comment_reply_link(__('Cancel reply', 'wicketpixie')) ?></p>
+			<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
+			<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php the_permalink(); ?>">logged in</a> to post a comment.</p>
+			<?php else : ?>
+			<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+				<div>
+					<?php if ( $user_ID ) : ?>
+					<p class="yourname" style="width:100%;">
+						Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Logout &raquo;</a>
+					</p>
+					<?php else : ?>
+					<p class="yourname">
+						<label for="author">Your Name: <?php if ($req) echo "*"; ?></label>
+						<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="30" tabindex="1" class="inputfield" />
+					</p>
+					<p class="email">
+						<label for="email">Email Address: <?php if ($req) echo "*"; ?></label>
+						<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="30" tabindex="2" class="inputfield" />
+					</p>
+					<p class="website">
+						<label for="url">Website:</label>
+						<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="30" tabindex="3" class="inputfield" />
+					</p>
+					<?php endif; ?>
+					<p>
+						<label for="comment">Comment:</label>
+						<textarea name="comment" id="comment-message" rows="10" cols="40" class="message" tabindex="4"></textarea>
+					</p>
+					<p>
+						<input type="image" src="<?php bloginfo('template_directory'); ?>/images/button-publish.jpg" name="submit" id="comment-button" alt="Publish My Comment" />
+						<?php comment_id_fields(); ?>	
+					</p>
+				</div>
+				<div class="clearer">&nbsp;</div>
+				<?php do_action('comment_form', $post->ID); ?>
+			</form>
+		</div>
 	</div>
 	<!-- /comment form -->
 	<?php endif; // If registration required and not logged in
