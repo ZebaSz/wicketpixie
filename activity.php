cfdd5d72dd95f3f46059eee25dee3eb5467237ac
Sebastian Szperling <zebaszp@gmail.com>
2011-03-24 23:42:11 -0300

    tables be gone! replaced with divs

--- activity.php
+++ activity.php
@@ -16,36 +16,32 @@ get_header(); ?>
 					<?php the_content(); ?>
 					<!-- activity -->
 					<div id="activity">
-						<div>
-							<?php $data= SourceAdmin::show_streams();
-							$page= ( isset( $_GET['page'] ) && ( $_GET['page'] > 0 ) ) ? intval( $_GET['page'] ) : 1;
-							$view= 50;
-							$results= array_chunk( $data, $view, true );
-							$test= range( 1, count( $results ) );
-							krsort( $test );
-							$last= array_slice( $test, 0, 1);
-							$get= $_GET; ?>
+						<?php $data= SourceAdmin::show_streams();
+						$page= ( isset( $_GET['page'] ) && ( $_GET['page'] > 0 ) ) ? intval( $_GET['page'] ) : 1;
+						$view= 50;
+						$results= array_chunk( $data, $view, true );
+						$test= range( 1, count( $results ) );
+						krsort( $test );
+						$last= array_slice( $test, 0, 1);
+						$get= $_GET;
+						$day= '';
+						$time_offset = get_option('gmt_offset') * 3600;
+						foreach( (array)$results[$page - 1] as $stream ) :
+						$source_data= SourceAdmin::source( $stream->name );
+						$local_date= $stream->date + $time_offset;
+						$this_day= date_i18n(__('F jS', 'wicketpixie'), $local_date);
+						if ( $day != $this_day ) : ?>
+						<h3><?php echo $this_day; ?></h3>
+						<?php endif; ?>
+						<div class="activity-entry">
+							<div class="activity-data">
+								<div class="activity-time"><?php echo date( get_option('time_format'), $local_date ); ?></div>
+								<div class="activity-content"><a href="<?php echo $stream->link; ?>" rel="nofollow"><?php echo $stream->content; ?></a></div>
+							</div>
+							<div class="activity-source"><a href="<?php echo $source_data->profile_url; ?>" rel="nofollow"><img src="<?php echo $source_data->favicon; ?>" alt="<?php echo $stream->name; ?>" /></a></div>
 						</div>
-						<table>
-							<?php $day= '';
-							$time_offset = get_option('gmt_offset') * 3600;
-							foreach( (array)$results[$page - 1] as $stream ) :
-							$source_data= SourceAdmin::source( $stream->name );
-							$local_date= $stream->date + $time_offset;
-							$this_day= date_i18n(__('F jS', 'wicketpixie'), $local_date);
-							if ( $day != $this_day ) : ?>
-							<tr>
-								<th colspan="3"><?php echo $this_day; ?></th>
-							</tr>
-							<?php endif; ?>
-							<tr>
-								<td class="activity-time"><?php echo date( get_option('time_format'), $local_date ); ?></td>
-								<td class="activity-content"><div><a href="<?php echo $stream->link; ?>" rel="nofollow"><?php echo $stream->content; ?></a></div></td>
-								<td class="activity-source"><a href="<?php echo $source_data->profile_url; ?>" rel="nofollow"><img src="<?php echo $source_data->favicon; ?>" alt="<?php echo $stream->name; ?>" /></a></td>
-							</tr>
-							<?php $day= $this_day;
-							endforeach; ?>
-						</table>
+						<?php $day= $this_day;
+						endforeach; ?>
 					</div>
 					<!-- /activity -->
 					<?php endwhile; ?>
