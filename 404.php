76742061fb593bcd42ade45e5ffcbd65de21c46f
Sebastian Szperling <zebaszp@gmail.com>
2011-03-26 00:00:25 -0300

    removed some plugins

--- 404.php
+++ 404.php
@@ -3,12 +3,8 @@
 			<div id="content">
 				<!-- page -->
 				<div class="page">
-					<?php if (get_option($optpre.'plug_aagoog404') == 'true' && function_exists("aa_google_404")) :
-					aa_google_404();
-					else : ?>
 					<h1><?php _e("We Can't Find the Droid You're Looking For", 'wicketpixie') ?></h1>
 					<p><?php printf(__("The page may have been removed or renamed. Be sure to check your spelling. If all else fails, you can %s, return to the %s, or try searching.", 'wicketpixie'), '<a href="javascript:history.back()">'.__('go back to the page you came from', 'wicketpixie').'</a>', '<a href="'.get_option('home').'/">'.__('homepage', 'wicketpixie').'</a>') ?></p>
-					<?php endif;?>
 				</div>
 				<!-- /page -->
 			</div>
