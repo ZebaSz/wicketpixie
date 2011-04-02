51ddef739d7749374ea496c087125691e96ede28
Sebastian Szperling <zebaszp@gmail.com>
2011-04-02 06:29:31 -0300

    restored old donations button on admin page

--- app/advert.php
+++ app/advert.php
@@ -1,10 +1,9 @@
-<!--
 <div id="admin-affiliates">
 	<h3>Donate to WicketPixie</h3>
+	<p>Well...I don't actually have a PayPal account or whatever is necessary to receive donations...yet. However, you may contribute to the original project. Hopefully, they'll release 2.0 sooner ;)</p>
 	<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
 		<input type="hidden" name="cmd" value="_s-xclick">
 		<input type="hidden" name="hosted_button_id" value="2767322">
 		<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt=""> <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
 	</form>
 </div>
--->
