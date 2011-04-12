abd7700602802b214add53b7fb9088b11710f99f
Sebastian Szperling <zebaszp@gmail.com>
2011-04-12 13:48:16 -0300

    moved animation function to wp-global.js added (and now serving) minified script

--- footer.php
+++ footer.php
@@ -11,8 +11,7 @@
 	<!-- wrapper -->
 	<!-- jQuery -->
 	<script type="text/javascript" src="<?php bloginfo('home'); ?>/wp-includes/js/jquery/jquery.js?ver=1.3.2"></script>
-	<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/wp-global.js"></script>
-	<script type="text/javascript">jQuery(document).ready(function($){$('#subscribe').hide();$("#topbar-subscribe a").toggle(function(){ $("#subscribe").animate({ height: "show", duration: 700, easing:"easeInQuad"});return false;},function(){$("#subscribe").animate({ height: "hide", duration: 700, easing:"easeOutQuad"});return false;});});</script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/wp-global-min.js"></script>
 <?php wp_footer(); ?>
 <?php echo "\n"; ?>
 <?php wp_customcode("footer"); ?>
--- js/wp-global.js
+++ js/wp-global.js
@@ -1,9 +1,16 @@
 jQuery(document).ready(function ($) {
-    //Add all your global onReady Functions here
-    setTimeout("hideLoading()", 500);
+	//Add all your global onReady Functions here
+	setTimeout("hideLoading()", 500);
+	$('#subscribe').hide();
+	$("#topbar-subscribe a").toggle(
+		function() { $("#subscribe").animate({ height: "show", duration: 700, easing:"easeInQuad"});
+		return false;
+	},
+		function() { $("#subscribe").animate({ height: "hide", duration: 700, easing:"easeOutQuad"});
+		return false;
+	});
 });
 
-
 function hideLoading() {
-    jQuery("#loadingFrame").css('display', 'none');
+	jQuery("#loadingFrame").css('display', 'none');
 }
