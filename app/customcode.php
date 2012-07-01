69949da02a62319ab5fa182a9c97582d09e99db7
Sebastián Szperling <zebaszp@gmail.com>
2012-07-01 01:19:55 -0300

    remove file_put_content, upload custom files instead
    improve admin page quality
    add gettext support

--- app/customcode.php
+++ app/customcode.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * WicketPixie v1.4
+ * WicketPixie v1.5
  * (c) 2006-2009 Eddie Ringle,
  *               Chris J. Davis,
  *               Dave Bates
  * Provided by Chris Pirillo
  *
- * (c) 2011 Sebastian Szperling
+ * (c) 2011-2012 Sebastian Szperling
  *
  * Licensed under the New BSD License.
  */
@@ -18,9 +18,47 @@
 **/
 define("CUSTOMPATH",get_template_directory()."/app/custom");
 /**
+ * Custom code functions array
+**/
+$custom_codes = array(
+	array(
+		'title' => __('Global Announcement','wicketpixie'),
+		'file' => 'global_announcement',
+		'desc' => __('Upload a file containing code that you\'d like to appear on the home page and all your posts as a global announcement.','wicketpixie')),
+	array(
+		'title' => __('Custom Header','wicketpixie'),
+		'file' => 'header',
+		'desc' => __('Upload a file containing code that you\'d like to appear between the &lt;head&gt; and &lt;/head&gt; tags of your site.','wicketpixie')),
+	array(
+		'title' => __('Custom Footer','wicketpixie'),
+		'file' => 'footer',
+		'desc' => __('Upload a file containing code that you\'d like to appear just before the &lt;/body&gt; tag of your site.','wicketpixie')),
+	array(
+		'title' => __('After-Home-Post','wicketpixie'),
+		'file'  => 'afterhomepost',
+		'desc' => __('Upload a file containing code that you\'d like to appear between the post content and post meta-data on your homepage.','wicketpixie')),
+	array(
+		'title' => __('After-Posts','wicketpixie'),
+		'file' => 'afterposts',
+		'desc' => __('Upload a file containing code that you\'d like to appear between the post content and post meta-data on individual posts.','wicketpixie')),
+	array(
+		'title' => __('After-Home-Meta','wicketpixie'),
+		'file' => 'afterhomemeta',
+		'desc' => __('Upload a file containing code that you\'d like to appear after the post meta data but before the Flickr Widget, Embedded Video, etc.','wicketpixie')),
+	array(
+		'title' => __('Custom Home Sidebar code','wicketpixie'),
+		'file' => 'homesidebar',
+		'desc' => __('Upload a file containing code that you\'d like to appear after the Tag Cloud section of the homepage sidebar.','wicketpixie')),
+	array(
+		'title' => __('Custom 404 code','wicketpixie'),
+		'file' => '404',
+		'desc' => __('Upload a file containing code that you\'d like to appear in a 404 page (this will replace the default message).','wicketpixie'))
+);
+/**
 * This is called in the admin page and every template that allows custom code.
 * It returns the custom code if any. If not, returns an HTML comment.
 **/
+
 function wp_customcode($file) {
 	if(file_exists(CUSTOMPATH) && file_exists(CUSTOMPATH ."/$file.php")) include(CUSTOMPATH ."/$file.php");
 	else echo "<!-- No custom code found, add code on the WicketPixie Custom Code admin page. -->";
@@ -39,68 +77,52 @@ class CustomCodeAdmin extends AdminPage {
 	* The admin page where the user enters the custom header code.
 	*/
 	function customcode_admin() {
-		if ( $_GET['page'] == basename(__FILE__) ) :
-			if (isset($_POST['action']) && $_POST['action'] == 'add') :
-				if (isset($_POST['file'])) :
-					// Check and make sure everything is created and writable
-					clearstatcache();
-					if(!file_exists(CUSTOMPATH)) mkdir(CUSTOMPATH,0777);
-					if(!file_exists(CUSTOMPATH ."/".$_POST['file'].".php")) touch(CUSTOMPATH ."/".$_POST['file'].".php");
-					// Write the submitted code to the file
-					file_put_contents(CUSTOMPATH ."/".$_POST['file'].".php",stripslashes($_POST['code']));
-				endif;
-			elseif (isset($_POST['action']) && $_POST['action'] == 'clear') :
-				if (isset($_POST['file'])) :
-					unlink(CUSTOMPATH."/".$_POST['file'].".php");
-				endif;
+		if ($_GET['page'] == basename(__FILE__) && isset($_POST['action']) && isset($_POST['file'])) :
+			$custom_file = CUSTOMPATH.'/'.$_POST['file'].'.php';
+			if ($_POST['action'] == 'add') :
+				// Check and make sure the folder exists and is writable
+				if(!file_exists(CUSTOMPATH)) mkdir(CUSTOMPATH,0777);
+				// Write the submitted code to the file
+				$uploaded = 'wicketpixie-custom-'.$_POST['file'];
+				if(move_uploaded_file($_FILES[$uploaded]['tmp_name'], $custom_file)) : ?>
+				<div id="message" class="updated fade"><p><strong><?php _e('Custom Code saved', 'wicketpixie'); ?></strong></p></div>
+				<?php else : ?>
+				<div id="message" class="error fade"><p><strong><?php _e('There has been an error with your upload', 'wicketpixie'); ?></strong></p></div>
+				<?php endif;
+			elseif ($_POST['action'] == 'clear') :
+				if(file_exists($custom_file)) :
+				unlink($custom_file); ?>
+				<div id="message" class="updated fade"><p><strong><?php _e('Custom Code cleared', 'wicketpixie'); ?></strong></p></div>
+				<?php else : ?>
+				<div id="message" class="error fade"><p><strong><?php _e('The file does not exist', 'wicketpixie'); ?></strong></p></div>
+				<?php endif;
 			endif;
-		endif;
-		if ( isset( $_REQUEST['add'] ) ) : ?>
-		<div id="message" class="updated fade"><p><strong>Custom Code saved.</strong></p></div>
-		<?php elseif(isset($_REQUEST['clear'])) : ?>
-		<div id="message" class="updated fade"><p><strong>Custom Code cleared.</strong></p></div>
-		<?php endif; ?>
+		endif; ?>
 			<div class="wrap">
 				<div id="admin-options">
-					<h2>Custom Code</h2>
-					<p>Click any title to enter special code (HTML, PHP, JavaScript) which will be included in the site template. If you want to delete any code, use the "Clear" buttons.</p>
-					<?php $custom_codes = array(
-						array('title' => 'Global Announcement','file' => 'global_announcement', 'desc' => 'on the home page and all your posts as a global announcement'),
-						array('title' => 'Custom Header','file' => 'header', 'desc' => 'between the &lt;head&gt; and &lt;/head&gt; tags of your site'),
-						array('title' => 'Custom Footer','file' => 'footer', 'desc' => 'just before the &lt;/body&gt; tag of your site'),
-						array('title' => 'After-Home-Post','file'  => 'afterhomepost', 'desc' => 'between the post content and post meta-data on your homepage'),
-						array('title' => 'After-Posts','file' => 'afterposts', 'desc' => 'between the post content and post meta-data on individual posts'),
-						array('title' => 'After-Home-Meta','file' => 'afterhomemeta', 'desc' => 'after the post meta data but before the Flickr Widget, Embedded Video, etc'),
-						array('title' => 'Custom Home Sidebar code','file' => 'homesidebar', 'desc' => 'after the Tag Cloud section of the homepage sidebar'),
-						array('title' => 'Custom 404 code','file' => '404', 'desc' => 'in a 404 page (this will replace the default message)'));
+					<h2><?php _e('Custom Code','wicketpixie'); ?></h2>
+					<p><?php _e('Here, you can upload php files containing any valid code (HTML, PHP, JavaScript) which will be included in the site template. If you want to delete any file, use the "Clear" buttons.','wicketpixie'); ?></p>
+					<?php global $custom_codes;
 					foreach ($custom_codes as $custom_code): ?>
-					<h3><a href="javascript:;" onmousedown="toggleDiv('edit_<?php echo $custom_code['file']; ?>');"><?php echo $custom_code['title']; ?></a></h3>
-					<p>Enter HTML markup, PHP code, or JavaScript that you would like to appear <?php echo $custom_code['desc']; ?>.</p>
-					<div id="edit_<?php echo $custom_code['file']; ?>" style="display: none;">
-						<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=customcode.php&amp;add=true" class="form-table">
+					<div style="clear: both">
+						<h3><?php echo $custom_code['title']; ?></h3>
+						<p><?php echo $custom_code['desc']; ?></p>
+						<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=customcode.php&amp;add=true" enctype="multipart/form-data">
 						<?php wp_nonce_field('wicketpixie-settings'); ?>
-							<h4>Edit <?php echo $custom_code['title']; ?></h4>
-							<p><textarea name="code" id="code" style="border: 1px solid #999999;" cols="80" rows="25" /><?php echo wp_customcode($custom_code['file'],true); ?></textarea></p>
-							<p class="submit">
-								<input name="save" type="submit" value="Save <?php echo $custom_code['title']; ?>" /> 
-								<input type="hidden" name="action" value="add" />
-								<input type="hidden" name="file" value="<?php echo $custom_code['file']; ?>" />
-							</p>
+							<input type="file" name="wicketpixie-custom-<?php echo $custom_code['file'];  ?>" style="float: left" />
+							<input type="submit" name="save" value="<?php _e('Save','wicketpixie'); ?>" class="button-primary" style="float: left" /> 
+							<input type="hidden" name="action" value="add" />
+							<input type="hidden" name="file" value="<?php echo $custom_code['file']; ?>" />
 						</form>
-						<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=customcode.php&amp;clear=true" class="form-table">
+						<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=customcode.php&amp;clear=true" enctype="multipart/form-data">
 						<?php wp_nonce_field('wicketpixie-settings'); ?>
-							<h4>Clear <?php echo $custom_code['title']; ?></h4>
-							<p>WARNING: This will delete all custom code you have entered, if you want to continue, click 'Clear <?php echo $custom_code['title']; ?>'</p>
-							<p class="submit">
-								<input name="clear" type="submit" value="Clear <?php echo $custom_code['title']; ?>" />
-								<input type="hidden" name="action" value="clear" />
-								<input type="hidden" name="file" value="<?php echo $custom_code['file']; ?>" />
-							</p>
+							<input type="submit" name="clear" value="<?php _e('Clear','wicketpixie'); ?>" class="button-secondary" />
+							<input type="hidden" name="action" value="clear" />
+							<input type="hidden" name="file" value="<?php echo $custom_code['file']; ?>" />
 						</form>
 					</div>
 				<?php endforeach; ?>
 				</div>
 			<?php include_once('advert.php'); ?>
-			<script language="javascript">function toggleDiv(divid){if(document.getElementById(divid).style.display == 'none'){document.getElementById(divid).style.display = 'block';}else{document.getElementById(divid).style.display = 'none';}}</script>
 	<?php }
 } ?>
