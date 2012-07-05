d0a41ae3fa311e31f4b531f69f6ad23ad7e5a15b
ZebaSz <zebaszp@gmail.com>
2012-07-05 20:49:49 -0300

    fix customizer previews

--- app/theme-customizer.js
+++ app/theme-customizer.js
@@ -36,7 +36,7 @@
 	});
 	wp.customize('wicketpixie_theme_titles_color',function(value) {
 		value.bind(function(to) {
-			$('#mid h1, #mid h2, #mid h3, #mid h4, #mid h5, #mid h6, #mid h1 a:link, #mid h1 a:visited, #mid h1 a:active, #mid h2 a:link, #mid h2 a:visited, #mid h2 a:active, #mid h3 a:link, #mid h3 a:visited, #mid h3 a:active, #mid h4 a:link, #mid h4 a:visited, #mid h4 a:active, #mid h5 a:link, #mid h5 a:visited, #mid h5 a:active, #mid h6 a:link, #mid h6 a:visited, #mid h6 a:active').css('color', to);
+			$('#mid #content h1, #mid #content h2, #mid #content h3, #mid #content h4, #mid #content h5, #mid #content h6, #mid #content h1 a:link, #mid #content h1 a:visited, #mid #content h1 a:active, #mid #content h2 a:link, #mid #content h2 a:visited, #mid #content h2 a:active, #mid #content h3 a:link, #mid #content h3 a:visited, #mid #content h3 a:active, #mid #content h4 a:link, #mid #content h4 a:visited, #mid #content h4 a:active, #mid #content h5 a:link, #mid #content h5 a:visited, #mid #content h5 a:active, #mid #content h6 a:link, #mid #content h6 a:visited, #mid #content h6 a:active').css('color', to);
 		});
 	});
 	wp.customize('wicketpixie_theme_sidebar_headings_color',function(value) {
@@ -46,7 +46,7 @@
 	});
 	wp.customize('wicketpixie_theme_content_links_color',function(value) {
 		value.bind(function(to) {
-			$('#mid a:link, #mid a:visited, #mid a:active, #content .comment h3 a:link, #content .comment h3 a:active, #content .comment h3 a:visited').css('color', to);
+			$('#mid #content a:link, #mid #content a:visited, #mid #content a:active, #content .comment h3 a:link, #content .comment h3 a:active, #content .comment h3 a:visited').not('h1 a, h2 a, h3 a, h4 a, h5 a, h6 a').css('color', to);
 			$('#mid a:hover, #mid h1 a:hover, #mid h2 a:hover, #mid h3 a:hover, #mid h4 a:hover, #mid h5 a:hover, #mid h6 a:hover, #mid #sidebar a:hover, #mid #sidebar h1 a:hover, #mid #sidebar h2 a:hover, #mid #sidebar h3 a:hover, #mid #sidebar h4 a:hover, #mid #sidebar h5 a:hover, #mid #sidebar h6 a:hover, #content .comment h3 a:hover').css('border-bottom', '1px solid ' + to);
 		});
 	});
