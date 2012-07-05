57327ceb4dce78ad7fb14bc65e2fdebfff997f6c
ZebaSz <zebaszp@gmail.com>
2012-07-05 20:50:43 -0300

    improve syntax

--- plugins/random-posts.php
+++ plugins/random-posts.php
@@ -4,21 +4,19 @@ Plugin Name: Random Posts
 */
 
 function query_random_posts($query) {
-	return query_posts($query . '&random=true');	
+	return query_posts($query.'&random=true');
 }
 
 class RandomPosts {
 	function orderby($orderby) {
-		if ( get_query_var('random') == 'true' )
+		if (get_query_var('random') == 'true')
 			return "RAND()";
-		else
-			return $orderby;
+		return $orderby;
 	}
 	function register_query_var($vars) {
 		$vars[] = 'random';
 		return $vars;
 	}
 }
-add_filter( 'posts_orderby', array('RandomPosts', 'orderby') );
-add_filter( 'query_vars', array('RandomPosts', 'register_query_var') );
-?>
+add_filter('posts_orderby', array('RandomPosts', 'orderby'));
+add_filter('query_vars', array('RandomPosts', 'register_query_var')); ?>
