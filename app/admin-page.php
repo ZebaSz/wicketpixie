13803861d1eb913c1729cbe4086f3d67b5936cf9
ZebaSz <zebaszp@gmail.com>
2012-07-05 23:43:21 -0300

    fix options not showing

--- app/admin-page.php
+++ app/admin-page.php
@@ -35,21 +35,17 @@ class AdminPage {
 			update_option($value['id'],$_POST[$value['id']]);
 		endif;
 	}
-	function extra_types_html($value,$checkdata) { ?>
-		<input name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php echo $checkdata; ?>" <?php if(($value['type'] == 'checkbox' || $value['type'] == 'radio') && $checkdata == 'true') { echo 'checked="checked"'; } ?> />
-		<?php if($value['type'] == 'checkbox') : ?><label for="<?php echo $value['id']; ?>">&nbsp;</label><?php endif;
-	}
 	function after_form() {
 	}
 	function save_hook() {
 	}
 	function save() {
 		foreach($this->arrays as $array) :
-			if((isset($array['name']) && $array['name'] == $_POST['group']) || (!isset($array['name']) && $_POST['group'] == '')) :
-				foreach($array as $value) :
-					if(is_array($value)) :
-						if($value['type'] == 'checkbox') :
-							if(isset($_POST[$value['id']])) :
+			if ((isset($array['name']) && $array['name'] == $_POST['group']) || (!isset($array['name']) && $_POST['group'] == '')) :
+				foreach ($array as $value) :
+					if (is_array($value)) :
+						if ($value['type'] == 'checkbox') :
+							if (isset($_POST[$value['id']])) :
 								update_option($value['id'],'true');
 							else :
 								update_option($value['id'],'false');
@@ -66,7 +62,7 @@ class AdminPage {
 	}
 	function add_page_to_menu() {
 		$this->request_check();
-		if($this->parent == null) :
+		if ($this->parent == null) :
 			$this->pagehook = add_menu_page($this->page_title,$this->page_name,'edit_themes',$this->filename,array($this,'page_display'),get_template_directory_uri() .'/images/wicketsmall.png');
 		else :
 			$this->pagehook = add_submenu_page($this->parent,$this->page_title,$this->page_name,'edit_themes',$this->filename,array($this,'page_display'));
@@ -87,7 +83,7 @@ class AdminPage {
 	}
 	function request_check() {
 		if (isset($_GET['page']) && isset($_POST['action'])) :
-			if($_GET['page'] == $this->filename && $_POST['action'] == 'save') :
+			if ($_GET['page'] == $this->filename && $_POST['action'] == 'save') :
 				check_admin_referer('wicketpixie-settings');
 				$this->save();
 			endif;
@@ -99,27 +95,31 @@ class AdminPage {
 				<h2><?php echo $this->page_name; ?></h2>
 				<?php echo $this->page_description; ?>
 				<?php foreach ($this->arrays as $array) : ?>
-				<?php if (!empty($array['name'])) echo '<h3>',$array["name"],'</h3>'; ?>
+				<?php if (!empty($array['name'])) echo '<h3>',$array['name'],'</h3>'; ?>
 				<?php if (!empty($array['desc'])) echo $array['desc']; ?>
 				<form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>?page=<?php echo $this->filename; ?>">
 					<?php wp_nonce_field('wicketpixie-settings'); ?>
 					<table class="form-table">
-						<?php foreach( $array as $value ) :
-						if(is_array($value)) : ?>
+						<?php foreach($array as $value) :
+						if (is_array($value)) :
+						$value['std'] = (!empty($value['std'])) ? $value['std'] : ''; ?>
 						<tr valign="top">
 							<th scope="row" style="font-size:12px;text-align:left;padding-right:10px;">
 								<acronym title="<?php echo $value['description']; ?>"><?php echo $value['name']; ?></acronym>
 								</th>
 							<td style="padding-right:10px;">
-								<?php $optdata = (get_option($value['id'])) ? get_option($value['id']) : (isset($value['std'])) ? $value['std'] : '';
+								<?php $optdata = (get_option($value['id'])) ? get_option($value['id']) : $value['std'];
 								if ($value['type'] == 'select') : ?>
 								<select name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>">
 									<?php foreach ($value['options'] as $option) : ?>
-									<option value="<?php echo $option; ?>" <?php if($optdata == $option) echo 'selected="selected"'; ?>><?php echo $option; ?></option>
+									<option value="<?php echo $option; ?>" <?php if ($optdata == $option) echo 'selected="selected"'; ?>><?php echo $option; ?></option>
 									<?php endforeach; ?>
 								</select>
-								<?php else :
-									$this->extra_types_html($value,$optdata);
+								<?php else : ?>
+								<input name="<?php echo $value['id']; ?>" id="<?php echo $value['id']; ?>" type="<?php echo $value['type']; ?>" value="<?php echo $optdata; ?>" <?php if (($value['type'] == 'checkbox' || $value['type'] == 'radio') && $optdata == 'true') echo 'checked="checked"'; ?>;/>
+								<?php if ($value['type'] == 'checkbox') : ?>
+								<label for="<?php echo $value['id']; ?>">&nbsp;</label>
+								<?php endif;
 								endif; ?>
 							</td>
 						</tr>
@@ -127,7 +127,7 @@ class AdminPage {
 						endforeach; ?>
 					</table>
 					<p class="submit">
-						<input name="save" type="submit" value="Save changes" />
+						<input type="submit" name="save" value="Save changes" />
 						<input type="hidden" name="action" value="save" />
 						<input type="hidden" name="group" value="<?php echo (isset($array['name'])) ? $array['name'] : ''; ?>" />
 					</p>
