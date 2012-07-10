b02bfa62ed49b1a929f5d2c5755548b13b259606
ZebaSz <zebaszp@gmail.com>
2012-07-10 00:57:28 -0300

    fix default theme options

--- app/theme-options.php
+++ app/theme-options.php
@@ -12,47 +12,47 @@
  */
 // Settings array - kept for compatibility for the time being
 $theme_options = array (
-	array(
+	'body_font' => array(
 		'name' => __('Body Font Family','wicketpixie'),
 		'id' => 'wicketpixie_theme_body_font',
 		'std' => 'Lucida Grande, Arial, Verdana, sans-serif',
 		'type' => 'font'),
-	array(
+	'headings_font' => array(
 		'name' => __('Headings Font Family','wicketpixie'),
 		'id' => 'wicketpixie_theme_headings_font',
 		'std' => 'Georgia, Times New Roman, Times, serif',
 		'type' => 'font'),
-	array(
+	'header_size' => array(
 		'name' => __('Logo Font Size (in pixels)','wicketpixie'),
 		'id' => 'wicketpixie_theme_header_size',
 		'std' => '40',
 		'type' => 'font'),
-	array(
+	'logo_color' => array(
 		'name' => __('Logo Text','wicketpixie'),
 		'id' => 'wicketpixie_theme_logo_color',
 		'std' => '#fff0a5',
 		'type' => 'color'),
-	array(
+	'description_color' => array(
 		'name' => __('Status/Description Text','wicketpixie'),
 		'id' => 'wicketpixie_theme_description_color',
 		'std' => '#9e6839',
 		'type' => 'color'),
-	array(
+	'titles_color' => array(
 		'name' => __('Titles/Content Headings','wicketpixie'),
 		'id' => 'wicketpixie_theme_titles_color',
 		'std' => '#b64926',
 		'type' => 'color'),
-	array(
+	'sidebar_headings_color' => array(
 		'name' => __('Sidebar Headings','wicketpixie'),
 		'id' => 'wicketpixie_theme_sidebar_headings_color',
 		'std' => '#8e2800',
 		'type' => 'color'),
-	array(
+	'content_links_color' => array(
 		'name' => __('Content Links','wicketpixie'),
 		'id' => 'wicketpixie_theme_content_links_color',
 		'std' => '#8e2800',
 		'type' => 'color'),
-	array(
+	'sidebar_links_color' => array(
 		'name' => __('Sidebar Links','wicketpixie'),
 		'id' => 'wicketpixie_theme_sidebar_links_color',
 		'std' => '#333',
@@ -82,21 +82,26 @@ add_action('customize_preview_init', 'wicketpixie_customize_preview');
 function wicketpixie_customize_preview() {
 	wp_enqueue_script('wicketpixie-customizer', get_template_directory_uri().'/app/theme-customizer.js', array('customize-preview'), false, true);
 }
+// Function called to output option if set, else output option default
+function wicketpixie_get_option($option) {
+	global $theme_options;
+	return (get_option('wicketpixie_theme_'.$option)) ? get_option('wicketpixie_theme_'.$option) : $theme_options[$option]['std'];
+}
 // Output settings
 add_action('wp_head', 'wicketpixie_theme_print_options');
 function wicketpixie_theme_print_options() { ?>
 	<style type='text/css'>
-		body, #comment-form input, #comment-form textarea, #content .comment h5, #mid #sidebar h5 {font-family: <?php echo get_option('wicketpixie_theme_body_font'); ?>;}
-		#logo {font-size: <?php echo get_option('wicketpixie_theme_header_size'); ?>px;}
-		#logo, #mid h1, #mid h2, #mid h3, #mid h4, #mid h5, #mid h6, #mid #sidebar h1, #mid #sidebar h2, #mid #sidebar h3, #mid #sidebar h4, #mid #sidebar h5, #mid #sidebar h6 {font-family: <?php echo get_option('wicketpixie_theme_headings_font'); ?>;}
-		#logo, #logo a:link, #logo a:visited, #logo a:active {color: <?php echo get_option('wicketpixie_theme_logo_color'); ?>;}
+		body, #comment-form input, #comment-form textarea, #content .comment h5, #mid #sidebar h5 {font-family: <?php echo wicketpixie_get_option('body_font'); ?>;}
+		#logo {font-size: <?php echo wicketpixie_get_option('header_size'); ?>px;}
+		#logo, #mid h1, #mid h2, #mid h3, #mid h4, #mid h5, #mid h6, #mid #sidebar h1, #mid #sidebar h2, #mid #sidebar h3, #mid #sidebar h4, #mid #sidebar h5, #mid #sidebar h6 {font-family: <?php echo wicketpixie_get_option('headings_font'); ?>;}
+		#logo, #logo a:link, #logo a:visited, #logo a:active {color: <?php echo wicketpixie_get_option('logo_color'); ?>;}
 		#logo a:hover {color: #fff;}
-		#description, #status p, #status a:link, #status a:active, #status a:visited {color: <?php echo get_option('wicketpixie_theme_description_color'); ?>;}
-		#content a:link, #content a:visited, #content a:active, #content .comment h3 a:link, #content .comment h3 a:active, #content .comment h3 a:visited {color: <?php echo get_option('wicketpixie_theme_content_links_color'); ?>;}
+		#description, #status p, #status a:link, #status a:active, #status a:visited {color: <?php echo wicketpixie_get_option('description_color'); ?>;}
+		#content a:link, #content a:visited, #content a:active, #content .comment h3 a:link, #content .comment h3 a:active, #content .comment h3 a:visited {color: <?php echo wicketpixie_get_option('content_links_color'); ?>;}
 		#mid h1, #mid h2, #mid h3, #mid h4, #mid h5, #mid h6, #mid #sidebar h1, #mid #sidebar h2, #mid #sidebar h3, #mid #sidebar h4, #mid #sidebar h5, #mid #sidebar h6 {font-weight: bold;}
-		#mid h1, #mid h2, #mid h3, #mid h4, #mid h5, #mid h6, #mid h1 a:link, #mid h1 a:visited, #mid h1 a:active, #mid h2 a:link, #mid h2 a:visited, #mid h2 a:active, #mid h3 a:link, #mid h3 a:visited, #mid h3 a:active, #mid h4 a:link, #mid h4 a:visited, #mid h4 a:active, #mid h5 a:link, #mid h5 a:visited, #mid h5 a:active, #mid h6 a:link, #mid h6 a:visited, #mid h6 a:active {color: <?php echo get_option('wicketpixie_theme_titles_color'); ?>;}
-		#mid #sidebar a:link, #mid #sidebar a:visited, #mid #sidebar a:active {color: <?php echo get_option('wicketpixie_theme_sidebar_links_color'); ?>;}
-		#mid a:hover, #mid h1 a:hover, #mid h2 a:hover, #mid h3 a:hover, #mid h4 a:hover, #mid h5 a:hover, #mid h6 a:hover, #mid #sidebar a:hover, #mid #sidebar h1 a:hover, #mid #sidebar h2 a:hover, #mid #sidebar h3 a:hover, #mid #sidebar h4 a:hover, #mid #sidebar h5 a:hover, #mid #sidebar h6 a:hover, #content .comment h3 a:hover {color: #000;border-bottom: 1px solid <?php echo get_option('wicketpixie_theme_content_links_color'); ?>;}
-		#mid #sidebar h1, #mid #sidebar h2, #mid #sidebar h3, #mid #sidebar h3 a:link, #mid #sidebar h3 a:visited, #mid #sidebar h3 a:active, #mid #sidebar h4, #mid #sidebar h5, #mid #sidebar h6 {color: <?php echo get_option('wicketpixie_theme_sidebar_headings_color'); ?>;}
+		#mid h1, #mid h2, #mid h3, #mid h4, #mid h5, #mid h6, #mid h1 a:link, #mid h1 a:visited, #mid h1 a:active, #mid h2 a:link, #mid h2 a:visited, #mid h2 a:active, #mid h3 a:link, #mid h3 a:visited, #mid h3 a:active, #mid h4 a:link, #mid h4 a:visited, #mid h4 a:active, #mid h5 a:link, #mid h5 a:visited, #mid h5 a:active, #mid h6 a:link, #mid h6 a:visited, #mid h6 a:active {color: <?php echo wicketpixie_get_option('titles_color'); ?>;}
+		#mid #sidebar a:link, #mid #sidebar a:visited, #mid #sidebar a:active {color: <?php echo wicketpixie_get_option('sidebar_links_color'); ?>;}
+		#mid a:hover, #mid h1 a:hover, #mid h2 a:hover, #mid h3 a:hover, #mid h4 a:hover, #mid h5 a:hover, #mid h6 a:hover, #mid #sidebar a:hover, #mid #sidebar h1 a:hover, #mid #sidebar h2 a:hover, #mid #sidebar h3 a:hover, #mid #sidebar h4 a:hover, #mid #sidebar h5 a:hover, #mid #sidebar h6 a:hover, #content .comment h3 a:hover {color: #000;border-bottom: 1px solid <?php echo wicketpixie_get_option('content_links_color'); ?>;}
+		#mid #sidebar h1, #mid #sidebar h2, #mid #sidebar h3, #mid #sidebar h3 a:link, #mid #sidebar h3 a:visited, #mid #sidebar h3 a:active, #mid #sidebar h4, #mid #sidebar h5, #mid #sidebar h6 {color: <?php echo wicketpixie_get_option('sidebar_headings_color'); ?>;}
 	</style>
 <?php } ?>
